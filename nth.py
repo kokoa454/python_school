966e5c714297884257a36e0c1153039aa46f751d
Inari Goro <qqnq7um9k@gmail.com>
2025-05-12 12:28:06 +0900

    added scatterPlot

--- nth.py
+++ nth.py
@@ -7,7 +7,7 @@ Created on Tue Apr 22 14:34:43 2025
 def nth(lst, n):
     dt = lst.copy()
 
-    for i in range(len(dt)):
+    for i in range(n):
         for j in range(i + 1, len(dt)):
             if dt[i] < dt[j]:
                 dt[i], dt[j] = dt[j], dt[i]
@@ -15,7 +15,7 @@ def nth(lst, n):
     return dt[n - 1]
 
 def nthWithoutCopy(lst, n):
-    for i in range(len(lst)):
+    for i in range(n):
         for j in range(i + 1, len(lst)):
             if lst[i] < lst[j]:
                 lst[i], lst[j] = lst[j], lst[i]
---
+++ scatterPlot.py
@@ -0,0 +1,40 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri May  9 14:36:04 2025
+
+@author: qqnq7
+"""
+
+import numpy as np 
+import matplotlib.pyplot as plt 
+from matplotlib.ticker import MultipleLocator
+
+ 
+
+#class1 data 
+
+mu1=np.array([2,2]) 
+
+V1=np.array([[1,0.01],[0.01,1]]) 
+
+class1=np.random.multivariate_normal(mu1,V1,size=30) 
+
+ 
+
+#class2 data 
+
+mu2=np.array([4,3]) 
+
+V2=np.array([[1,0.01],[0.01,1]]) 
+
+class2=np.random.multivariate_normal(mu2,V2,size=30) 
+
+#scatter plot coontrol
+plt.scatter(class1[:, 0], class1[:, 1], label='Class1', c='red', marker="*")
+plt.scatter(class2[:, 0], class2[:, 1], label='Class2', c='blue', marker="o")
+ax = plt.gca()
+ax.yaxis.set_major_locator(MultipleLocator(0.5))
+plt.xlabel('x1')
+plt.ylabel('x2')
+plt.legend()
+plt.show()
