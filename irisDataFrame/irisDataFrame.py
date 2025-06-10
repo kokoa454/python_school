ed600f86bcdd48c878b6a88593cd6c3e6304aa05
Inari Goro <qqnq7um9k@gmail.com>
2025-06-10 20:23:34 +0900

    added irisDataFrame

---
+++ irisDataFrame/irisDataFrame.py
@@ -0,0 +1,16 @@
+import pandas as pd 
+from sklearn import datasets 
+
+iris = datasets.load_iris() 
+
+df = pd.DataFrame(iris.data, columns=iris.feature_names) 
+
+try:
+    with open("irisC24036.csv", "w") as f:
+        df.describe().to_csv(f) 
+except Exception as e:
+    print("csv error")
+    print(e)
+
+# 問1 sepal length(cm), sepal width(cm), petal length(cm), petal width(cm)
+# 問2 596個
